185294fffad46a37156d8ce0fa1800bea4788ea9
Nader Safari <80418026+Nader1995@users.noreply.github.com>
2022-11-14 10:42:43 +0330

    Add files via upload

---
+++ components/button.jsx
@@ -0,0 +1,16 @@
+import react, {Component} from "react";
+
+class Button extends Component {
+
+    render() {
+
+        return (
+        <div>
+            <h1>Here is the button:</h1>
+            <button>Button</button>
+        </div>
+        );
+    }
+}
+
+export default Button;
