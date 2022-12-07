cf772bc122eb91d6c8d6c3aeb07ac160274f04ac
Nader Safari <80418026+Nader1995@users.noreply.github.com>
2022-12-07 19:42:43 +0330

    Update connectToMetaMask.jsx

--- src/components/connectToMetaMask.jsx
+++ src/components/connectToMetaMask.jsx
@@ -16,50 +16,20 @@ export  default function ConnectToMetaMask() {
     const [tokenBalance, setTokenBalance] = useState("");
 
     window.$provider= new ethers.providers.Web3Provider(window.ethereum);
-    // var provider = new ethers.providers.Web3Provider(window.ethereum);
 
     const ERC20ABI = require('./erc20.abi.json');
 
-    // to solve: argument type is not assignable to parameter type
-    // const [provider, setProvider] = useState(null);
-
     const connectWalletHandler = async () => {
-
-            // to solve:  RPC Error: invalid argument 0: hex string has length 0, want 40 for common.Address
-            // setProvider(new ethers.providers.Web3Provider(window.ethereum));
 
             const accountName = await window.ethereum.request({method: 'eth_requestAccounts'});
             setAccount(accountName[0]);
             console.log(accountName[0]);
-
-            // await window.ethereum.request({method:'eth_requestAccounts'})
-            //     .then(result => {
-            //         console.log(result);
-            //         setAccount(result[0]);
-            //     })
-
-            // await window.ethereum.request({
-            //     method: 'eth_getBalance',
-            //     params: [account, 'latest']
-            // })
-            //     .then(res => {
-            //         const balanceInEth = ethers.utils.formatEther(res);
-            //         setBalance(balanceInEth);
-            //      })
     }
-
-    // const getBalance = async (account) => {
-    //     const result = await window.$provider.getBalance(account);
-    //     setBalance((ethers.utils.formatEther(result)));
-    // }
 
     useEffect(() => {
 
-        // For token:
-
         if (account){
-            // console.log("hello");
-            // declare the data fetching function
+            
             const fetchBalanceData = async () => {
                 const data = await window.$provider.getBalance(account);
                 setBalance(ethers.utils.formatEther(data));
@@ -74,31 +44,13 @@ export  default function ConnectToMetaMask() {
                 console.log(`token balance: ${tokenBalance} BAT`);
             }
 
-            // call the function
             fetchBalanceData()
-                // make sure to catch any error
                 .catch(console.error);
 
             fetchTokenData()
                     .catch(console.error)
         }
     }, [account, balance, tokenAddress, tokenBalance, ERC20ABI])
-
-    // useEffect(() => {
-    //
-    //         if (account) {
-    //
-    //             // setBalance(ethers.utils.formatEther(window.$provider.getBalance(account)));
-    //
-    //             // console.log(window.$provider.getBalance(account));
-    //
-    //             window.$provider.getBalance(account)
-    //                 .then(result =>{
-    //                     console.log(result);
-    //                     setBalance(ethers.utils.formatEther(result));
-    //                 })
-    //         }
-    //     });
 
     return(
         <div style={divStyle}>
